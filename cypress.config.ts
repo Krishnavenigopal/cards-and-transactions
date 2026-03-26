b67ea6ece72afa2b4c4c9d63a324458e1e47dcb1
Krishnavenigopal <krishnaveniv90@gmail.com>
2026-03-26 21:09:11 +0100

    add the preview url to as env variable - cypress

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "cypress";
  
 export default defineConfig({
   e2e: {
-    baseUrl: "http://localhost:5173",
+    baseUrl: process.env.CYPRESS_BASE_URL ?? "http://localhost:5173",
     specPattern: "cypress/e2e/**/*.cy.ts",
     supportFile: false,
     viewportWidth: 1280,
